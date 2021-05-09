3b3cad2fdef065c07b21482d1f86aa12e0239b52
Brandon Lyons <lyonsbp@umich.edu>
2021-05-09 17:42:52 -0400

    Add header

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <div class="flex bg-brandGreen justify-around text-white py-2">
+      <div class="">
+        <div>support@fitin.com</div>
+      </div>
+      <div>
+        <span>F</span>
+        <span>LI</span>
+        <span>IG</span>
+      </div>
+    </div>
+    <div class="flex justify-around bg-white py-4">
+      <span>Logo</span>
+      <nav class="text-gray-600 font-semibold">
+        <a href="/">Fitness</a>
+        <a href="/">About Us</a>
+        <a href="/">FAQ</a>
+        <button class="btn primary ml-5">JOIN</button>
+        <button class="btn secondary">LOGIN</button>
+      </nav>
+    </div>
+  </header>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brandGreen: '#0E6978',
+        brandOrange: '#FF3D00',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
